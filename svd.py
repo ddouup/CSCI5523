66b9098006d247f956de4ae81749d11810ef28fd
DDOU <ddouup@gmail.com>
2018-09-27 21:45:15 -0500

    generate dimension reducted data

--- svd.py
+++ svd.py
@@ -10,14 +10,17 @@ def SVD(df, dimension):
 
 	svd = TruncatedSVD(n_components=dimension)
 	T = svd.fit_transform(data)
-	output = pd.DataFrame()
-	name = dimension+'d_mnist_test'
+
+	output = pd.concat([labels, pd.DataFrame(T)], axis=1, ignore_index=True)
+	print(output)
+	name = 'data/'+str(dimension)+'d_mnist_test.csv'
 	output.to_csv(name, header=False, index=False)
 
 def main():
-	df = pd.read_csv('data/test.csv', header=None)
+	df = pd.read_csv('data/mnist_test.csv', header=None)
 
-	for i in [5, 10, 20, 40]
+	mylist = [5, 10, 20, 40]
+	for i in mylist:
 		SVD(df, i)
 	
 if __name__ == '__main__':
